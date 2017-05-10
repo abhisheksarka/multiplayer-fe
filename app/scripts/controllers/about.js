d3a13958b11ed5bc174e95edf1312634b8dd3ff8
abhisheksarka <abhisheksarka@gmail.com>
2017-05-10 15:22:51 +0530

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lokalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lokalApp
+ */
+angular.module('lokalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
