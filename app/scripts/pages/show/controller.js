4259aefa76cc217f2989e67f75088634133e7e0e
abhisheksarka <abhisheksarka@gmail.com>
2017-05-13 12:43:55 +0530

    url rewritting

--- app/scripts/pages/show/controller.js
+++ app/scripts/pages/show/controller.js
@@ -15,7 +15,23 @@
     ps.selectedCity = $routeParams.city;
     ps.selectedType = $routeParams.type;
 
-  }
+
+    function updateLocation() {
+      $location.update_path('show/' + ps.selectedCity + '/' + ps.selectedType);
+    };
+
+    $scope.$watch(angular.bind(ps, function () {
+      return ps.selectedCity;
+    }), function (newVal, oldVal) {
+      updateLocation();
+    });
+
+    $scope.$watch(angular.bind(ps, function () {
+      return ps.selectedType;
+    }), function (newVal, oldVal) {
+      updateLocation();
+    });
+  };
 
   angular
     .module('app.pages.show')
